a2886df2aca1663c4b81842af5cbda0675c816b8
indyix <indyix@qq.com>
2020-04-22 01:08:47 +0800

    tuple set dict

--- pythonbasic/basic3_datetype_tuple.py
+++ pythonbasic/basic3_datetype_tuple.py
@@ -1,13 +1,51 @@
-# coding = utf-8
-# auth: SSRGray
-# tuple: 有序数组，可重复，用()
+"""
+    @ 功能：笔记：tuple
+    @ author：古城
+    @ create：20200422 01:00
+"""
+# tuple: 不可变序列，元素可重复，特点是“内部元素有序不可变”，相比列表可以作为字典的键
 # 数据类型？储存数据用的
-#特性
+# 使用场景：保存程序中不可修改的内容
+
+# 1.创建元组：通过赋值语句创建：小括号不是必须的，单元素要加逗号，
+tupleb = ()                     # 空元组
+tuple0 = ("t",)                 # 单元素元组
+tuple1 = (1,0,0.0002,'str',True,True,False,(1,),[2,],{'key1':'15'},{'set1','set2'})     # 使用()
+tuple2 = 1,0,0.0002,'str',True,True,False,(1,),[2,],{'key1':'15'},{'set1','set2'}  # 使用 ,
+tuple3 = tuple(range(3, 10, 2))
+tuple4 = tuple([1, "a", True])                 # 使用tuple()函数和可迭代序列
+tuple5 = tuple(x for x in range(10))            # 使用元组推导式
+tuple3_3 = tuple(tuple(x for x in range(3)) for y in range(3))  #套用元组推导式：二维元组
+print("二维元组：", tuple3_3)
+# 2.删除元组：
+del tuple1
+# 3.编辑元组：
+    # 3.1 元组元素不可编辑   如tuple2[5] = "edittuple"会报错，列表的切片赋值    .append()   .extend()   .insert()   .remove()   .pop()等操作元素的方法也不能用
+    # 3.2 但元组本身是可以“编辑”的
+tuple2 = 1,0,0.0002,'str',True,True,False,(1,),[2,],{'key1':'15'},{'set1','set2'},"after"   # 通过重新赋值编辑整个元组
+tuple2 = tuple2 + ("add",)              # 通过 + 进行元组整体间的拼接（不能拼接元组和其他序列）
+# 4.访问元组：
+print(tuple2)                   # 通过元组名访问整体
+print(tuple2[4])                # 通过索引访问元素
+print(tuple2[3:7:1])            # 通过切片访问元素
+    # 遍历元素： enumerate() 将一个可遍历的数据对象组合为一个具有下标的索引序列,序列的元素是二元素的tuple
+print(list(enumerate(tuple2)))
+print(type(list(enumerate(tuple2))[1]))
+for index0, item0 in enumerate(tuple2):
+    if index0 == 9:
+        print(item0)
+
+l3 = [1,2,3,4]
+l3[0:3] = [3,4,5]
+print("列表可以用切片进行赋值", l3)
+
+
+# 其他（预习视频）
 ''' 1：空元组,长度0'''
 # t1 = ()
 # print(type(t1))
 # print(len(t1))
-''' 2：一元素元组要在后面加上逗号，不然会按元素识别'''
+''' 2：一元素元组要在后面加上逗号，不然会按元素的数据类型进行识别'''
 # t2 = (1,)
 # print(type(t2))
 # print(len(t2))
@@ -23,6 +61,6 @@ t4 = (1,0,0.0002,'str',True,True,False,(1,),[2,],{'key1':'15'},{'set1','set2'})
 ##获取元素的索引值.index(元素)
 # print(t4.index(0.0002))     #结果：2
 # print(t4.count(1))          #结果是：3：把1当做True来计数了
-# print(t4.count(0))          #结果是：2：把0当做Flase来计数了
+# print(t4.count(0))          #结果是：2：把0当做False来计数了
 
 
--- pythonbasic/basic4_datatype_dict.py
+++ pythonbasic/basic4_datatype_dict.py
@@ -1,8 +1,89 @@
-# -*- conding: utf-8 -*-
-# @Author   : SSRGray
-''' 字典：无序，元素为键值对key:value，key唯一且不可变且不能用list、dict,value无限制'''
+"""
+    @ 功能：笔记：dict、set
+    @ author：古城
+    @ create：20200422 01:00
+"""
 
-'''特点：'''
+# 字典dict：也称关联数组、散列表(hash)
+# 特点：“无序可变”，元素为键值对  key:value
+#       没有索引（无序）
+#       key唯一且不可变且不能用list、dict, 出现两次则只记住后一个值
+#       value无限制（可变）
+
+# 1. 创建字典
+    # 通过 {}，键值间用:
+dict1 = {1: "阿尔法", 2: "赛普西斯"}
+    # 通过dict()函数
+        # 使用 = 构成多个键值对，键不能用数字
+dict2 = dict(username="伊卡洛斯", pw= "123456")
+print(dict2)
+        # 使用一个字典型元组((k1,v1), (k2, v2),(k3, v3))
+dict3 = dict(((1, "阿尔法"), (2, "赛普西斯"), (3, "艾娃")))
+print("两元元组构成键值对", dict3)
+        # 使用zip()函数:zip(list1, list2)
+dict4 = dict(zip([1, 2, 3], ["a", "b", "c"]))
+print("zip:", dict4)
+    # 空字典
+dict0a = {}  # 空字典
+dict0b = dict.fromkeys([1,2,3,4])   #空值字典
+# 2. 删除字典
+del dict1       # 删除字典
+dict2.clear()   # 清空字典
+p3 = dict3.pop(2)    # 删除指定键的元素，并返回被删除的键值对的值
+print("pop删除指定键值对并返回键值对的值",dict3,p3)
+p4 = dict4.popitem()  # 删除一个元素，并返回该键值对元组
+print("popitem删除键值对并返回键值对元组：", dict4, p4)
+# 3. 访问字典
+dict5 = {1: "asd", 2: "qwe", 3: "zxc"}
+print("字典名：", dict5)        # 通过字典名
+print("通过键名访问字典元素的键值：", dict5[3])  # 通过键名
+print(dict5.get(5, "没有这个键名"))           # 通过dict.get(key[,无key时返回值])
+    # 遍历字典
+for item in dict5.items():
+    print(item)     # .items()      每个item是一个元组
+for k1, v1 in dict5.items():
+    print(k1, v1)   # .items()      每个k1，v1是元组的元素
+print(dict5.keys())     # .keys()   返回keys
+print(dict5.values())   # .values() 返回values
+# 4. 删增改字典元素
+del dict5[1]
+dict5[4] = "asdadd"
+dict5[4] = "asdmodify"
+print("删增改字典元素", dict5)
+# 5. 字典推导式
+dict6 = {i: i**i for i in range(4)}   # {0:1, 1:1, 2:4, 3:9 }    注：0**0处理为1
+print(dict6)
+
+
+# 集合set和frozenset，用{}
+# 特点：无序、元素唯一
+# set是可变集合、frozenset是不可变集合
+
+# 1. 创建set:元素唯一、无序
+set1 = {1, 1, "战列巡洋舰", "虚空光辉舰", True}   # 使用{}
+set0 = set()    #空集合                        # 空集合要用set()，不能用{}
+set2 = set([1, 1, 3, 5, 6])                     # set(iterable),推荐方法
+print("set元素唯一且无序：", set0, set1, set2)
+# 2. 增删集合的元素
+set2.add("added")       #.add(元素) 增加元素
+# set2.add([1, 5, 6])            # 不能使用可迭代对象作为.add()的参数
+del set1
+set2.pop()             # .pop() 对于有索引的对象不指定参数时等价于.pop(-1)，对于dict需要指定key，set则不指定
+print("set2被pop后",set2)
+set2.remove(3)
+set2.clear()
+list00 = [1,2,3,4,5,6,7]
+print(list00.pop())
+# 集合运算
+set3 = {5, 6, "a", "b", True}
+set4 = {"a", "b", 1, "中国雷达"}
+print("交集：",set3 & set4)  # 交集，True→1
+print("并集：",set3 | set4)  # 并集，1→True
+print("差集：",set4 - set3)  # 差集
+print("对称差集：",set3 ^ set4)  # 对称差集
+
+
+# 其他（预习视频）
 '''1：空字典'''
 # d = {}
 # print(type(d))
